43163433cfbdcf100833c84d491e63909cc385f1
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-12-19 11:37:44 -0500

    Hola mundo y variables en Python desde cero

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario:
+# Nuestro hoa mundo en Python
+print("Hola Python")
+print('Hola Python')
+"""
+Este es un 
+comentario
+en varias líneas
+"""
+'''
+Este es otro 
+comentario
+en varias líneas
+'''
+# Consultar el tipo de dato
+print(type("Hola Python")) # tipo str
+print(type(5)) # tipo int
+print(type(1.5)) # tipo float
+print(type(1 + 1j)) # tipo complex
+print(type(True)) # tipo bool
---
+++ 01_variables.py
@@ -0,0 +1,4 @@
+# Variables
+
+my_variable = "My String variable"
+print(my_variable)
