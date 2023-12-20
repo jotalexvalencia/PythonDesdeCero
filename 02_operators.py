216368259635de5316d69575c7731773e5d9fb96
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-12-20 15:37:30 -0500

    02_operators -> operadores

---
+++ 02_operators.py
@@ -0,0 +1,42 @@
+### Operadores Aritméticos ###
+
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(10 / 3)
+print(2 ** 3) 
+print(2 ** 3 + 3 - 7 / 1 // 4) 
+
+print("Hola" + "Python") 
+print("Hola" + str(5)) 
+print("Hola" * 5) 
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+print("Hola" >= "Zola") # Ordenación alfabética
+
+### Operadores Lógicos ###
+print(" Operadores Lógicos")
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" < "Python")
+print(not(3 > 4))
